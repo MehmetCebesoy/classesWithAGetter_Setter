71ed00aba35f5c8f71c3f74bf4bf529fc58ece25
Mehmet CEBESOY <mhmtcbsy@gmail.com>
2022-02-13 23:16:39 +0300

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    //attribute:özellik
+    //Aşağıdakilere attribute veya field denilebilir
+    int id;
+    String name;
+    String description;
+    double price;
+    int stockAmount;
+
+
+
+}
---
+++ src/ProductManager.java
@@ -0,0 +1,6 @@
+public class ProductManager {
+    public void Add(Product product){
+        //JDBS
+        System.out.println("Ürün eklendi "+product.name);
+    }
+}
